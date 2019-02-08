68cfd0bb0a7edbaacf23f88337f642bfe70ac13d
sumanra <sumanra@users.noreply.github.com>
2019-02-07 23:22:55 -0500

    Pybank Amount change is fixed date is still pending

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,42 +18,45 @@ with open(budgetdata_csv, newline="") as csvfile:
     total = 0
     months = 0
     average = 0.0
-    a = ["", 0]
-    b = 0.0
+
     greatest = ["", 0]
     least = ["",0]
-    average_change = 0.0
+   # max_index = 0
+    #least_index = 0
+    change_in_profit = [0.0]
     lenght = 0
-#    def average(row):
-#        length = len(row)
- #       average_change = 0.0
-#        for rows in row:
-#            average_change += rows
-#    return average_change / (length-2)
-    for row in csvreader:
-        #print(row)
-        lenght = len(row)
-        total = total + int(row[1])
-        months= months+1
-        a = row[1]
-        b = [s.split(',') for s in a]
-        average_change = float(average_change) - float(row[1])
-        average = average_change/(lenght-2)
-        try:
-            print(average)
-        except ZeroDivisionError:
-            print ("You can't divide by zero!")
-        #average_ = int(row[months+1]))-int(row[1])/(months-1)   
-        #average = (total/months)
-        
-       
+    average1 = 0
+    Profit_Loss_in_previous_row = 0
+    change_in_profit_Sum = 0
 
-        if int(row[1]) > int(greatest [1]):
-            greatest = row
+    for row in csvreader:
+               
+        total+= int(row[1])
+        months= months+1
         
-        if int(row[1]) < int(least [1]):
-            least = row
-#average = average_change/(row- 2)    
+       # print(Profit_Loss_in_previous_row)
+        if (months > 1) :
+            #change_in_profit.append((int(row[1]))
+            change_in_profit.append( (int(row[1]) - Profit_Loss_in_previous_row))
+
+        Profit_Loss_in_previous_row = int(row[1])
+   
+       #finding greatest and least increase i  profit and loss
+        #if int(row[1]) == int(greatest[1]):
+        #    greatest[0] = row[0]
+        
+        #if int(row[1]) < int(least [1]):
+        #    least[0] = row[0]
+
+change_in_profit_Sum = sum(change_in_profit)
+#print(change_in_profit_Sum)
+average = round((change_in_profit_Sum/(months-1)),2)
+greatest[1] = max(change_in_profit)
+#finding the max index
+#max_index = change_in_profit.index(greatest[1])
+#finding the min index
+least[1] = min(change_in_profit)
+
 # Printing everything
 print("```text")
 print("Financial Analysis")
@@ -61,7 +64,7 @@ print("--------------------------------")
 print(f"Total Months: {months}")
 print(f"Total: ${total}")
 print(f"Average  Change: ${average}")
-print(f"Greatest Increase in Profits:{greatest}")
-print(f"Greatest Decrease in Profits:{least}")
+print(f"Greatest Increase in Profits:  {greatest[0]}  (${greatest[1]})")
+print(f"Greatest Decrease in Profits:  {least[0]}  (${least[1]})")
 print("```")
 
