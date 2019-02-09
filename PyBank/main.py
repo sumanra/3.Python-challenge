9452b6a3fd44b35c7e6c73199a315540e15344ae
sumanra <sumanra@users.noreply.github.com>
2019-02-09 12:45:26 -0500

    All done

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,12 +18,12 @@ with open(budgetdata_csv, newline="") as csvfile:
     total = 0
     months = 0
     average = 0.0
-
+    date = [""]
     greatest = ["", 0]
     least = ["",0]
    # max_index = 0
     #least_index = 0
-    change_in_profit = [0.0]
+    change_in_profit = [0]
     lenght = 0
     average1 = 0
     Profit_Loss_in_previous_row = 0
@@ -38,6 +38,8 @@ with open(budgetdata_csv, newline="") as csvfile:
         if (months > 1) :
             #change_in_profit.append((int(row[1]))
             change_in_profit.append( (int(row[1]) - Profit_Loss_in_previous_row))
+            date.append(row[0])
+
 
         Profit_Loss_in_previous_row = int(row[1])
    
@@ -50,12 +52,20 @@ with open(budgetdata_csv, newline="") as csvfile:
 
 change_in_profit_Sum = sum(change_in_profit)
 #print(change_in_profit_Sum)
+#Average  Change: and round it to two decimal
 average = round((change_in_profit_Sum/(months-1)),2)
+#Greatest Increase in Profits:
 greatest[1] = max(change_in_profit)
 #finding the max index
-#max_index = change_in_profit.index(greatest[1])
-#finding the min index
+max_index = change_in_profit.index(greatest[1])
+#Greatest Increase in Profits Dates:
+max_change_date = date[max_index]
+#Greatest Decrease in Profits:
 least[1] = min(change_in_profit)
+#finding the min index
+min_index = change_in_profit.index(least[1])
+#Greatest Decrease in Profits dates:
+min_change_date = date[min_index]
 
 # Printing everything
 print("```text")
@@ -64,7 +74,7 @@ print("--------------------------------")
 print(f"Total Months: {months}")
 print(f"Total: ${total}")
 print(f"Average  Change: ${average}")
-print(f"Greatest Increase in Profits:  {greatest[0]}  (${greatest[1]})")
-print(f"Greatest Decrease in Profits:  {least[0]}  (${least[1]})")
+print(f"Greatest Increase in Profits:  {max_change_date}  (${greatest[1]})")
+print(f"Greatest Decrease in Profits:  {min_change_date}  (${least[1]})")
 print("```")
 
