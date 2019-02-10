ae90bcde61de553b50849fff84d06ac7ac9cc99a
sumanra <sumanra@users.noreply.github.com>
2019-02-09 20:22:10 -0500

    output to a text file as well

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,14 @@
-# Modules
+## Bank Budget Analysis ##
 import os
 import csv
 
+def print_and_write(file, data):
+    print(data, end="")
+    file.write(data)
+
 budgetdata_csv = os.path.join("budget_data.csv")
 
+#Open and read the CSV
 with open(budgetdata_csv, newline="") as csvfile:
 
 # CSV reader specifies delimiter and variable that holds contents
@@ -12,46 +17,34 @@ with open(budgetdata_csv, newline="") as csvfile:
  # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
   
-    #print(f"CSV Header: {csv_header}")
-
     # assigning the value
     total = 0
     months = 0
     average = 0.0
-    date = [""]
-    greatest = ["", 0]
+    date = []
+    greatest = ["",0]
     least = ["",0]
-   # max_index = 0
-    #least_index = 0
     change_in_profit = [0]
-    lenght = 0
-    average1 = 0
     Profit_Loss_in_previous_row = 0
     change_in_profit_Sum = 0
-
+# Calculation
     for row in csvreader:
-               
+        #Total profits/losses for the period       
         total+= int(row[1])
+        #The number of months
         months= months+1
-        
-       # print(Profit_Loss_in_previous_row)
+        #List of dates
+        date.append(row[0])
+       
         if (months > 1) :
             #change_in_profit.append((int(row[1]))
             change_in_profit.append( (int(row[1]) - Profit_Loss_in_previous_row))
-            date.append(row[0])
-
-
+            
         Profit_Loss_in_previous_row = int(row[1])
    
-       #finding greatest and least increase i  profit and loss
-        #if int(row[1]) == int(greatest[1]):
-        #    greatest[0] = row[0]
-        
-        #if int(row[1]) < int(least [1]):
-        #    least[0] = row[0]
-
+# Sum of total change in profit/loss
 change_in_profit_Sum = sum(change_in_profit)
-#print(change_in_profit_Sum)
+
 #Average  Change: and round it to two decimal
 average = round((change_in_profit_Sum/(months-1)),2)
 #Greatest Increase in Profits:
@@ -67,14 +60,16 @@ min_index = change_in_profit.index(least[1])
 #Greatest Decrease in Profits dates:
 min_change_date = date[min_index]
 
+#Create the path for the filename
+budgetdata_output=os.path.join("budgetdata.txt")
 # Printing everything
-print("```text")
-print("Financial Analysis")
-print("--------------------------------")
-print(f"Total Months: {months}")
-print(f"Total: ${total}")
-print(f"Average  Change: ${average}")
-print(f"Greatest Increase in Profits:  {max_change_date}  (${greatest[1]})")
-print(f"Greatest Decrease in Profits:  {min_change_date}  (${least[1]})")
-print("```")
-
+with open(budgetdata_output, "w", newline="") as textfile:
+    print_and_write(textfile, "```text \n")
+    print_and_write(textfile, "Financial Analysis \n")
+    print_and_write(textfile, "-------------------------------- \n")
+    print_and_write(textfile, f"Total Months: {months} \n")
+    print_and_write(textfile, f"Total: ${total} \n")
+    print_and_write(textfile, f"Average  Change: ${average} \n")
+    print_and_write(textfile, f"Greatest Increase in Profits:  {max_change_date}  (${greatest[1]}) \n")
+    print_and_write(textfile, f"Greatest Decrease in Profits:  {min_change_date}  (${least[1]}) \n")
+    print_and_write(textfile, "``` \n")
