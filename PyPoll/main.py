9c256ed5453ecba826958bbf3cd756dac12285a7
sumanra <sumanra@users.noreply.github.com>
2019-02-09 21:11:17 -0500

    output to a text file as well

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,11 @@
-# Modules
+# #Election Result Analysis
 import os
 import csv
+
+def print_and_write(file, data):
+    print(data, end="")
+    file.write(data)
+
 
 electiondata_csv = os.path.join("election_data.csv")
 
@@ -15,66 +20,40 @@ with open(electiondata_csv, newline="") as csvfile:
     #print(f"CSV Header: {csv_header}")
 #  Assigning the value
     total_votes = 0
-    Khan_Counter = 0
-    Correy_Counter = 0
-    Li_Counter = 0
-    Tooley_Counter = 0
-    percent_Khan = 0.000
-    percent_Correy = 0.000
-    percent_Li = 0.000
-    percent_Tooley = 0.000
 
+    candidate_vote_counts = {} 
+    #Creating dictionary to read all the votes, count the total and find unique candidates
     for row in csvreader:
-        #Total number of votes
-        total_votes = total_votes +1
-        #  The percentage of votes each candidate won
-        #  The total number of votes each candidate won
+        candidate_name = row[2]
+        if candidate_name in candidate_vote_counts:
+            candidate_vote_counts[candidate_name] += 1
+        else:
+            candidate_vote_counts[candidate_name] = 1
+    total_votes = sum(candidate_vote_counts.values())
 
-  
-        if str(row[2]) == "Khan" :
-            Khan_Counter = Khan_Counter + 1
-            percent_Khan = round((int(Khan_Counter) / int(total_votes)*100), 3)
+    #The path for the filename
+    output_file = os.path.join("electiondata.txt")
+
+    #Write data to a .txt file
+    with open(output_file, "w", newline="") as textfile:
+        print_and_write(textfile, "```text \n")
+        print_and_write(textfile, "Election Results \n")
+        print_and_write(textfile, "-------------------------------- \n")
+        print_and_write(textfile, f"Total Votes: {total_votes} \n")
+        print_and_write(textfile, "-------------------------------- \n")
+
+        for candidate_name in candidate_vote_counts.keys():     
+
+            #Calculate the percentage of votes for each candidate
+            percent_votes = candidate_vote_counts[candidate_name] / total_votes * 100
+            #Printing and writing
+            print_and_write(textfile, "%s: %.3f %% (%i)\n" % (candidate_name, percent_votes, candidate_vote_counts[candidate_name]))
         
-        elif str(row[2]) == "Correy" :
-            Correy_Counter = Correy_Counter + 1
-            percent_Correy = round((int(Correy_Counter) / int(total_votes)*100), 3)
+        #Winner equals the key with the largest value
+        winner= max(candidate_vote_counts, key=candidate_vote_counts.get)
 
-        elif str(row[2]) == "Li" :
-            Li_Counter = Li_Counter + 1
-            percent_Li = round((int(Li_Counter) / int(total_votes)*100), 3)
-            
 
-        elif str(row[2]) == "O'Tooley" :
-            Tooley_Counter = Tooley_Counter + 1
-            percent_Tooley = round((int(Tooley_Counter) / int(total_votes)*100), 3)
-        else :
-            print("No one from the list")
-
-# Checking the Winner of the Election
-    if Khan_Counter > Correy_Counter and Khan_Counter > Li_Counter and Khan_Counter > Tooley_Counter :
-        Winner = str("Khan")
-    elif Correy_Counter > Li_Counter and Correy_Counter > Tooley_Counter :
-        Winner = str("Correy")
-    elif Li_Counter > Tooley_Counter:
-        Winner = str("Li")
-    else:
-        Winner = str("O'Tooley")
-        
-        # Determine percent of review left to 2 decimal places
-        #percent = round(int(row[6]) / int(row[5]), 2)
-        #review_percent.append(percent)
-
-# Printing everything
-print("```text")
-print("Election Results")
-print("--------------------------------")
-print(f"Total Votes: {total_votes}")
-print("--------------------------------")
-print(f"Khan: {percent_Khan}% ({Khan_Counter}) ")
-print(f"Correy: {percent_Correy}% ({Correy_Counter})")
-print(f"Li: {percent_Li}% ({Li_Counter})")
-print(f"O'Tooley: {percent_Tooley}% ({Tooley_Counter})")
-print("--------------------------------")
-print(f"Winner:{Winner}")
-print("--------------------------------")
-print("```")
+        print_and_write(textfile, "-------------------------------- \n")
+        print_and_write(textfile, f"Winner: {winner} \n")
+        print_and_write(textfile, "-------------------------------- \n")
+        print_and_write(textfile, "``` \n")
